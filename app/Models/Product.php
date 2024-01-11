608de78ade415394d5d32be4fc7588591c629476
Jocelyn Llamas <mjlldlt@hotmail.com>
2024-01-11 15:41:44 -0600

    Eloquent Relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,10 +11,10 @@ class Product extends Model
 	
 	protected $fillable = ['name', 'price', 'category_id'];
 	
-	// public function category()
-	// {
-	// 	return $this->belongsTo(Category::class);
-	// }
+	public function category()
+	{
+		return $this->belongsTo(Category::class);
+	}
 }
 
 /*
