bca1969786ff01b8dad80a5df2b93c4a8e370996
Eding Muhamad <masaruedo@yahoo.com>
2024-01-11 14:55:07 +0700

    Upgrade to Laravel 10

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'price' => $this->faker->randomDigitNot(0) * 100,
+            'category_id' => Category::pluck('id')->random()
+        ];
+    }
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::factory(5)->create();
+        Product::factory(20)->create();
+    }
+}
